0bbdbdc2f79c2bc680d79d6235b81b08952b6072
harrj396 <ahletmethink@gmail.com>
2022-11-11 12:41:11 -0500

    Forgot to updatee connectionJS with new host

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ const Sequelize = require('sequelize');
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
+      host: '	h1use0ulyws4lqr1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,

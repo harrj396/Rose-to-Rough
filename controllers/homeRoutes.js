08245eedd502ed4c0ec907ea59d35fe6dde1aa66
harrj396 <ahletmethink@gmail.com>
2022-11-15 13:50:44 -0500

    Cleaned files and added data to handlebar homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
-const { Post, User, Review } = require('../models');
+const {User, Review } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
     // Get all post and JOIN with user data
-    const reviewData = await Post.findAll({
+    const reviewData = await Review.findAll({
       include: [
         {
           model: User,
@@ -15,11 +15,11 @@ router.get('/', async (req, res) => {
     });
 
     // Serialize data so the template can read it
-    const post = reviewData.map((post) => post.get({ plain: true }));
+    const reviews = reviewData.map((review) => review.get({ plain: true }));
 
     // Pass serialized data and session flag into template
     res.render('homepage', { 
-      post, 
+      reviews, 
       logged_in: req.session.logged_in 
     });
   } catch (err) {
--- models/Review.js
+++ models/Review.js
@@ -1,5 +1,4 @@
 const { Model, DataTypes } = require('sequelize');
-const { Post } = require('.');
 const sequelize = require('../config/connection');
 
 class Review extends Model {}

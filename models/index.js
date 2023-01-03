507a50a60aa616e970710473fcf09a591c274eda
oliverLo78 <oliverberto@gmail.com>
2023-01-03 18:04:24 -0500

    added user has one license to associations

--- models/index.js
+++ models/index.js
@@ -1,5 +1,15 @@
 const User = require('./User');
 const Review = require('./Review');
+const License = require('./License');
+
+User.hasOne(License, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+License.belongsTo(User, {
+  foreignKey: 'user_id',
+});
 
 // Define a User as having many review, thus creating a foreign key in the 
 // 'review' table

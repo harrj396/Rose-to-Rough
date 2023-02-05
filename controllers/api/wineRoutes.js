d69a77cd7b0086308b7623d438da481b993c1231
oliverLo78 <oliverberto@gmail.com>
2023-02-05 12:07:43 -0500

    added the wineRoutes to create/delete user

--- controllers/api/wineRoutes.js
+++ controllers/api/wineRoutes.js
@@ -1,30 +1,37 @@
-// const { Model, DataTypes } = require('sequelize');
-// const sequelize = require('../config/connection');
+const router = require('express').Router();
+// Include the Review model with the other imports
+const { User, Review, Wine } = require('../../models');
 
-// // create our Location model
-// class Wine extends Model {}
+// CREATE a user
+router.post('/', async (req, res) => {
+  try {
+    const wineData = await Wine.create({
+      reader_id: req.body.trip_id,
+    });
+    res.status(200).json(reviewData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
-// // create fields/columns for Location model
-// .init(
-//   {
-//     id: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//       primaryKey: true,
-//       autoIncrement: true
-//     },
-//     location_name: {
-//       type: DataTypes.STRING,
-//       allowNull: false
-//     }
-//   },
-//   {
-//     sequelize,
-//     timestamps: false,
-//     freezeTableName: true,
-//     underscored: true,
-//     modelName: 'location'
-//   }
-// );
+// DELETE a user
+router.delete('/:id', async (req, res) => {
+  try {
+    const wineData = await Wine.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
 
-// module.exports = Location;
+    if (!wineData) {
+      res.status(404).json({ message: 'No user found with that id!' });
+      return;
+    }
+
+    res.status(200).json(wineData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;

00db16f6b172b55e52ce548c206563fb9a1f9f7b
oliverLo78 <oliverberto@gmail.com>
2023-02-06 11:31:21 -0500

    destroyed api folders

---
+++ controllers/reviewRoutes.js
@@ -0,0 +1,73 @@
+const router = require("express").Router();
+const { Review, User, Wine } = require("../models");
+
+// We are using hardcoded data here, where would our our data usually come from? Remember -
+// we haven't yet set up a database or Sequelize in our app
+const reviews = [
+  {
+    id: 1,
+    title: "Homewood",
+    taster_name: "Ana Dasilva",
+    country: "US",
+    variety: "Zinfandel Port",
+    winery: "Homewood Winery",
+    province: "Sonoma",
+    region_1: "Sonoma Country",
+    description:
+      "An amazing port with aromas and flavors of dark skinned summer time fruits that you eat over the sink. Pair it with a chocolate raspberry torte and your favorite vanilla ice cream",
+  },
+  {
+    id: 2,
+    title: "Fuentevina",
+    taster_name: "Oliver",
+    country: "Spain",
+    variety: "Cabernet Sauvignon",
+    winery: "Fuentevina Winery",
+    province: "Castilla y Leon",
+    region_1: " Extremadura region of Spain",
+    description:
+      "Fresh citrus fruit flavors lead on the palate with a nice underlying note of tropical fruit.",
+  },
+];
+
+// GET all reviews
+router.get("/", async (req, res) => {
+  res.render("homepage", { reviews });
+});
+
+// GET a single review
+router.get("/review/:id", async (req, res) => {
+  return res.render("review", reviews[req.params.num - 1]);
+});
+
+// CREATE a review
+router.post("/", async (req, res) => {
+  try {
+    const reviewData = await Review.create(req.body);
+    res.status(200).json(reviewData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// DELETE a review
+router.delete("/:id", async (req, res) => {
+  try {
+    const reviewData = await Review.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!reviewData) {
+      res.status(404).json({ message: "No review found with this id!" });
+      return;
+    }
+
+    res.status(200).json(reviewData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,61 +1,21 @@
-const path = require('path');
+// Dependencies
 const express = require('express');
-const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
-// import sequelize connection
-const sequelize = require('./config/connection');
+const path = require('path');
+const hbs = exphbs.create({});
 
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
+// Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ helpers });
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {
-    maxAge: 300000,
-    httpOnly: true,
-    secure: false,
-    sameSite: 'strict',
-  },
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
-// App level middleware
-app.use(session(sess));
-
-// App level middleware to Inform Express.js -- which template engine to use
+// Set Handlebars as the default template engine.
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-// App level middleware -- for body parsing
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./controllers/reviewRoutes'));
 
-// App level middleware -- for routing
-app.use(routes);
-
-app.get('/', (req, res) => {
-  //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
-  res.render('main', {layout : 'index'});
-  });
-
-// sync sequelize models to the database, then turn on the server
-// Force false so data doesn't get dropped on every sync
-sequelize.sync({ force: false }).then
-(() => {
-  app.listen(PORT, () => console.log
-  (`App listening on port ${PORT}!`));
+// Starts the server to begin listening
+app.listen(PORT, () => {
+  console.log('Server listening on: http://localhost:' + PORT);
 });

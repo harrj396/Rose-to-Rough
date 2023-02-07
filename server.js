15356a6326aa5fd3f139631c2b7b60fb9bc361b9
oliverLo78 <oliverberto@gmail.com>
2023-02-06 22:31:21 -0500

    finally got review route to load but no data renders

--- server.js
+++ server.js
@@ -7,15 +7,18 @@ const hbs = exphbs.create({});
 // Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 3001;
+const sequelize = require('./config/connection');
 
 // Set Handlebars as the default template engine.
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('./controllers/reviewRoutes'));
 
-// Starts the server to begin listening
-app.listen(PORT, () => {
-  console.log('Server listening on: http://localhost:' + PORT);
-});
+sequelize.sync({ force: false }).then(() => {
+  // Starts the server to begin listening
+  app.listen(PORT, () => console.log('Server listening on: http://localhost:' + PORT));
+  });
